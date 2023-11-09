21576994a2d2d019183a9d83b5d7602d3939665d
NHarrison25 <41209807+NHarrison25@users.noreply.github.com>
2023-11-08 22:44:23 -0500

    Remove deprecated ACE self-actions

---
+++ CfgVehicles.hpp
@@ -0,0 +1,49 @@
+class CfgVehicles
+{
+	class Man;
+	class CAManBase: Man
+	{
+		class ACE_SelfActions
+		{
+			class ACE_Equipment
+			{
+				class TFN_BCKL
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+				class TFN_UNBCKL
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+				class TFN_IRON
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+				class TFN_IROFF
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+				class TFN_COMS
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+				class TFN_NOCOMS
+				{
+					displayName = "";
+					condition = "false";
+					statement = "";
+				};
+			};
+		};
+	};
+};
